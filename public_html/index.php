fc498ce6ea7af735e883616a6d3d7092c92c6cb8
mattpetts <mattpetts23@gamil.com>
2019-04-11 22:24:01 +0100

    page styled

--- public_html/index.php
+++ public_html/index.php
@@ -6,7 +6,7 @@
 
         <!--CSS Includes-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+        <link rel="stylesheet" href="/assets/css/style.min.css" />
         <!--Javascrip Includes-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -17,32 +17,61 @@
     <?php include($_SERVER["DOCUMENT_ROOT"] . '/assets/php/classes/generator.php') ?>
         <div class="container">
             <div class="jumbotron">
-                <h1>Generate a secure password for free</h1>
+                <h1>Generate a secure password</h1>
                 <form id="pref-form">
-                    <input type="number" name="length" value="6"/>
-                    <label>Include capital letters</label><input type="checkbox" id="capitals" name="capitals" value="yes"/>
-                    <label>Include numbers</label><input type="checkbox" id="numbers" name="numbers" value="yes"/>
-                    <label>Include symbols</label><input type="checkbox" id="symbols" name="symbols" value="yes"/>
-                    <input class="btn btn-primary" type="submit" value="Get a password">
+                    <div class="form-element elem-first">
+                        <label>Length</label><input type="number" min="6" name="length" value="8"/>
+                    </div>
+                    <div class="form-element elem-check">
+                        <label>Include capital letters</label><input type="checkbox" id="capitals" name="capitals" value="yes"/>
+                    </div>
+                    <div class="form-element elem-check">
+                        <label>Include numbers</label><input type="checkbox" id="numbers" name="numbers" value="yes"/>
+                    </div>
+                    <div class="form-element elem-check">
+                        <label>Include symbols</label><input type="checkbox" id="symbols" name="symbols" value="yes"/>
+                    </div>
+                    <div class="form-element">
+                        <input type="submit" value="Get a password">
+                    </div>
                 </form> 
-                <h3 id="output"></h3>
+                <div class="output-wrapper">
+                    <input type="text" value="" placeholder="Your New Password" id="output"/>
+                    <button onClick="copy()" id="copy">Copy</button>
+                </div>
+            </div>
+            <div class="leaderboard">
+                <img src="/assets/images/leaderboard.jpg" />
             </div>
         </div>
         <script>
             $('#pref-form').submit(function(event){
                 event.preventDefault();
-
-              
                 $.ajax({
                     type: "POST",
                     url: "/assets/php/classes/generator.php",
                     data: $('#pref-form').serialize(),
                     success: function(resultData){
-                       $('#output').html(resultData);
+                       $('#output').val(resultData);
+                       $('#copy').css('display', 'block');
                     }
                 });
             });
-        </script>]
+
+            function copy() {
+                /* Get the text field */
+                var copyText = document.getElementById('output');
+
+                /* Select the text field */
+                copyText.select();
+
+                /* Copy the text inside the text field */
+                document.execCommand("copy");
+
+                /* Alert the copied text */
+                alert("Copied the password " + copyText.value);
+            }
+        </script>
     </body>
 
 </html>
